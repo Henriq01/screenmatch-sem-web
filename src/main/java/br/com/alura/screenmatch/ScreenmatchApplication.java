3c2a490263ad2351732b1157cd66ecdaf5517d5e
Luann Henrique <LuannHenri@outlook.com>
2026-08-13 11:09:51 -0300

    Adicionando a classe Pincipal e a classe main

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,9 +1,7 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
+import br.com.alura.screenmatch.principal.Principal;
 import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,25 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
+    @Override
+    public void run(String... args) throws Exception {
+        var consumoApi = new ConsumoApi();
 
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6b90c889");
-		//System.out.println(json);
+        Principal principal = new Principal();
+        principal.exibeMenu();
 
-		ConverteDados conversor = new ConverteDados();
 
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&Episode=2&apikey=6b90c889");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-	}
+    }
 }
